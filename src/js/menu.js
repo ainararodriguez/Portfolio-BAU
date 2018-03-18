63179c8f077912532ec1f470b75500ea0e89899b
Ainara Rodriguez <ainaraoiartzun@gmail.com>
2018-03-18 20:41:43 +0100

    add a lot of changes and new files

---
+++ src/js/menu.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    var ultimaPosicionScroll = $(document).scrollTop();
+    $(window).scroll(function(event){
+        var posicionScroll = $(document).scrollTop();
+        if(posicionScroll>ultimaPosicionScroll && posicionScroll > $(".mainHome").outerHeight()+$("nav").outerHeight()){
+            $("nav").css("visibility", "hidden");
+            $("nav").css("opacity", "0");
+
+        }
+        else{
+            $("nav").css("visibility", "visible");
+            $("nav").css("opacity", "1");
+        }
+        ultimaPosicionScroll = posicionScroll;
+    })
+    $(document).on("scroll", alHacerScroll);
+});
+
+function alHacerScroll(event){
+    var posicionScroll = $(document).scrollTop();
+    ajustarPosicionNavegador(posicionScroll);
+    var ultimaPosicionScroll = posicionScroll;
+}
+
+function ajustarPosicionNavegador(posicionScroll){
+    if (posicionScroll > $(".mainHome").outerHeight()){
+        $("nav").css("position", "fixed");
+        $(".ultimosProyectos").css("margin-top", "78px");
+    }
+
+    else{
+        $("nav").css("position", "relative");
+        $(".ultimosProyectos").css("margin-top", "0px");
+    }
+}
